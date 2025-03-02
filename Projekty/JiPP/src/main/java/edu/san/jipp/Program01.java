0410d7494894b1ec765a5c18a1d9217e47b3cf69
Konrad Grzanek <kongra@gmail.com>
2025-03-02 11:02:54 +0100

    JiPP 1st commit

---
+++ Projekty/JiPP/src/main/java/edu/san/jipp/Program01.java
@@ -0,0 +1,10 @@
+// © 2025 Konrad Grzanek <kongra@gmail.com>
+package edu.san.jipp;
+
+public class Program01 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ Zjazdy B/JiPP/Wykład 1/Program.java
@@ -0,0 +1,24 @@
+// DEFINICJA 1
+// Obiekt: BYT POSIADAJĄCY TOŻSAMOŚĆ
+
+// f: R -> R
+// f(x) = 2x+3
+
+// g: R -> R
+// g(y) = 2y+3
+
+class Program {
+
+  static double f(x) {
+    return 2*x + 3;
+  }
+
+  static double g(y) {
+    return 2*y + 3;
+  }
+
+  public static void main(String... args) {
+    System.out.println("Witaj Świecie");
+  }
+
+}
